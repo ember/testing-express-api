57d03351a3511c030334718786d2cd9159393f6b
Pedro Fragoso <ember@pfragoso.org>
2018-03-01 22:11:22 +0000

    start deploying the infra

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,3 +8,18 @@ docker run \
     --volume $(pwd)/terraform:/terraform \
     -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
     chrisns/docker-terragrunt init -reconfigure
+
+docker run \
+    --rm \
+    --workdir /terraform \
+    --volume $(pwd)/terraform:/terraform \
+    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+    -e APP_VERSION=$TRAVIS_COMMIT \
+    chrisns/docker-terragrunt plan -out plan -var "api_version=$APP_VERSION"
+
+docker run \
+    --rm \
+    --workdir /terraform \
+    --volume $(pwd)/terraform:/terraform \
+    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+    chrisns/docker-terragrunt apply plan
