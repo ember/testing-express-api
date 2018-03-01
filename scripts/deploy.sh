a0bf7aebd65227fc742c19d81c4f8e64062af576
Pedro Fragoso <ember@pfragoso.org>
2018-03-01 22:06:07 +0000

    use terragrunt and not terraforms

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,5 +7,4 @@ docker run \
     --workdir /terraform \
     --volume $(pwd)/terraform:/terraform \
     -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-    hashicorp/terraform \
     chrisns/docker-terragrunt init -reconfigure
